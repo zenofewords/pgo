2719079142e5c1f99ae7b81a2f64c6c48c9b4eeb
Dominik Zen <zen.dominik@gmail.com>
2017-03-05 19:55:56 +0100

    Display pokemon stats, moves

--- pgo/views.py
+++ pgo/views.py
@@ -9,7 +9,16 @@ from pgo.models import Pokemon
 
 class PokemonListView(ListView):
     model = Pokemon
+    paginate_by = 300
+
+    def get_queryset(self):
+        return self.model.objects.select_related('primary_type', 'secondary_type')
 
 
 class PokemonDetailView(DetailView):
     model = Pokemon
+
+    def get_queryset(self):
+        return self.model.objects.select_related(
+            'primary_type', 'secondary_type').prefetch_related(
+            'quick_moves', 'cinematic_moves')

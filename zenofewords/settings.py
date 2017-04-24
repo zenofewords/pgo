3eeb9cb60b889cec5792e19f656f439fc4ac993b
Dominik Zen <zen.dominik@gmail.com>
2017-04-24 22:41:59 +0200

    Update allowed hosts

--- zenofewords/settings.py
+++ zenofewords/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv('DEBUG', False))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.dominikzen.com', '178.62.114.50']
 INTERNAL_IPS = ['127.0.0.1', ]
 
 # Application definition

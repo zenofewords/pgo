3127806cc0fb6b4b3c6ab51a0c2d371777fcc8ef
Dominik Zen <zen.dominik@gmail.com>
2017-05-08 10:43:09 +0200

    Add column ordering, cleanup templates

--- pgo/views.py
+++ pgo/views.py
@@ -5,19 +5,48 @@ from django.views.generic import (
 )
 
 from pgo.models import (
-    Pokemon, Move, MoveSet, Type,
+    Pokemon, Move, Moveset, Type, DEFAULT_ORDER
 )
+
+
+class SortMixin(ListView):
+    def get(self, request, *args, **kwargs):
+        sort_by = request.GET.get('sort_by')
+        sort_key = request.GET.get('sort_key')
+
+        if sort_by:
+            if sort_key == sort_by:
+                self.sort_by = ('-{}'.format(sort_by),)
+            else:
+                self.sort_by = (sort_by,)
+        else:
+            self.sort_by = self._get_default_order()
+        return super(SortMixin, self).get(request, args, kwargs)
+
+    def _get_default_order(self):
+        return DEFAULT_ORDER[self.model.__name__]
+
+    def get_queryset(self):
+        if self.sort_by:
+            return self.model.objects.order_by(*self.sort_by)
+        return self.model.objects
+
+    def get_context_data(self, **kwargs):
+        context = super(SortMixin, self).get_context_data(**kwargs)
+        context['sort_key'] = '&amp;'.join(self.sort_by)
+        return context
 
 
 class PGoHomeView(TemplateView):
     template_name = 'pgo/pgo_home.html'
 
 
-class PokemonListView(ListView):
+class PokemonListView(SortMixin):
     model = Pokemon
 
     def get_queryset(self):
-        return self.model.objects.select_related('primary_type', 'secondary_type')
+        queryset = super(PokemonListView, self).get_queryset()
+        return queryset.select_related('primary_type', 'secondary_type')
 
 
 class PokemonDetailView(DetailView):
@@ -31,15 +60,16 @@ class PokemonDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(PokemonDetailView, self).get_context_data(**kwargs)
 
-        context['movesets'] = MoveSet.objects.filter(pokemon=self.object)
+        context['movesets'] = Moveset.objects.filter(pokemon=self.object)
         return context
 
 
-class MoveListView(ListView):
+class MoveListView(SortMixin):
     model = Move
 
     def get_queryset(self):
-        return self.model.objects.select_related('move_type')
+        queryset = super(MoveListView, self).get_queryset()
+        return queryset.select_related('move_type')
 
 
 class MoveDetailView(DetailView):
@@ -54,13 +84,17 @@ class TypeDetailView(DetailView):
     model = Type
 
 
-class MoveSetListView(ListView):
-    model = Pokemon
+class MovesetListView(SortMixin):
+    model = Moveset
     template_name = 'pgo/moveset_list.html'
-    paginate_by = 75
+    paginate_by = 300
+
+    def get_queryset(self):
+        queryset = super(MovesetListView, self).get_queryset()
+        return queryset.select_related('pokemon')
 
 
-class MoveSetDetailView(DetailView):
+class MovesetDetailView(DetailView):
     model = DetailView
 
 

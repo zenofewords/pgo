80561ce21cf2c69c3e9534778db76c0c1d2800bc
Dominik Zen <zen.dominik@gmail.com>
2017-06-29 12:54:43 +0200

    Allow 3 decimals

--- pgo/models.py
+++ pgo/models.py
@@ -78,7 +78,7 @@ class TypeEffectivness(models.Model):
 
 
 class TypeEffectivnessScalar(NameMixin):
-    scalar = models.DecimalField(max_digits=4, decimal_places=2)
+    scalar = models.DecimalField(max_digits=4, decimal_places=3)
 
     def __str__(self):
         return '{0} ({1})'.format(self.name, self.scalar)

6968348f831a984b3c716389efeacd989c5ee7ef
Dominik Zen <zen.dominik@gmail.com>
2017-10-14 14:37:00 +0200

    Add team list view

--- registry/urls.py
+++ registry/urls.py
@@ -3,9 +3,12 @@ from __future__ import unicode_literals
 
 from django.conf.urls import url
 
-from registry.views import TrainerListView
+from registry.views import (
+    TrainerListView, TeamListView,
+)
 
 
 urlpatterns = (
-    url(r'^trainers/$', TrainerListView.as_view(), name='trainers'),
+    url(r'^$', TrainerListView.as_view(), name='trainers'),
+    url(r'^teams/$', TeamListView.as_view(), name='teams'),
 )
--- registry/views.py
+++ registry/views.py
@@ -3,8 +3,14 @@ from __future__ import unicode_literals
 
 from django.views.generic import ListView
 
-from registry.models import Trainer
+from registry.models import (
+    Team, Trainer,
+)
 
 
 class TrainerListView(ListView):
     model = Trainer
+
+
+class TeamListView(ListView):
+    model = Team

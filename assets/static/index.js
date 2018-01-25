fe036cf1ca77ffcc48eb023536002e8ebd14855d
Dominik Zen <zen.dominik@gmail.com>
2018-01-25 02:37:55 +0100

    Revert get param for static files

--- assets/static/index.js
+++ assets/static/index.js
@@ -1,5 +1,5 @@
 require('./pgo/attack_proficiency.js')
 require('./pgo/attack_proficiency.css')
 
-require('./pgo/main.js?v=1')
-require('./pgo/main.css?v=1')
+require('./pgo/main.js')
+require('./pgo/main.css')

1a335a66efef6dd212c123fe40a7a52511073efe
Dominik Zen <zen.dominik@gmail.com>
2018-03-08 22:28:20 +0100

    Use ga tag only in prod

--- zenofewords/templatetags/zenofewords_tags.py
+++ zenofewords/templatetags/zenofewords_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 
 from zenofewords.models import Navigation
 
@@ -13,6 +14,11 @@ def navigation_tag(context, navigation_slug, subnav=False):
         'current_url': path.replace('/', '').replace(navigation_slug, ''),
         'navigation': Navigation.objects.filter(slug=navigation_slug).first()
     }
+
+
+@register.inclusion_tag('zenofewords/tags/google_analytics.html')
+def google_analytics_tag():
+    return {'render': not settings.DEBUG}
 
 
 @register.filter

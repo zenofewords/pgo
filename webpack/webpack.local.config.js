c8ad1623d0abf88fb41adabaa4e092a83fad1a66
Dominik Zen <zen.dominik@gmail.com>
2018-03-13 21:46:06 +0100

    Update npm packages and webpack config

--- webpack/webpack.local.config.js
+++ webpack/webpack.local.config.js
@@ -1,22 +1,36 @@
 var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
 var config = require('./webpack.base.config.js')
+var CompressionPlugin = require("compression-webpack-plugin")
 
 
-config.entry = [
-  'webpack-dev-server/client?http://localhost:3000',
-  '../assets/static/index'
-]
+config.entry = {
+  main: '../assets/static/index',
+}
 config.output.path = require('path').resolve('./assets/bundles')
 config.output.publicPath = 'http://localhost:3000/assets/bundles/'
 
 config.plugins = config.plugins.concat([
-  new webpack.NoErrorsPlugin(),
+  new webpack.NoEmitOnErrorsPlugin(),
   new BundleTracker({filename: 'webpack/webpack-stats.json'}),
+    new webpack.optimize.UglifyJsPlugin({
+    mangle: true,
+    compressor: {
+      warnings: false,
+      pure_getters: true,
+      unsafe: true,
+      unsafe_comps: true,
+      screw_ie8: true,
+    },
+    output: {
+      comments: false,
+    },
+  }),
+  new webpack.NoEmitOnErrorsPlugin(),
 ])
 
 config.module.loaders.push(
-  { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel'] }
+  { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' }
 )
 
 module.exports = config
--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,27 +1,44 @@
 var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
 var config = require('./webpack.base.config.js')
+var CompressionPlugin = require("compression-webpack-plugin")
 
 
 config.output.filename = '[name].min.js',
 config.output.path = require('path').resolve('./assets/prod')
 
 config.plugins = config.plugins.concat([
-  new BundleTracker({filename: 'webpack/webpack-stats-prod.json'}),
-
+  new BundleTracker({
+    filename: 'webpack/webpack-stats-prod.json',
+  }),
   new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-  }}),
+    'process.env.NODE_ENV': '"production"',
+  }),
   new webpack.optimize.UglifyJsPlugin({
+    mangle: true,
     compressor: {
-      warnings: false
-    }
-  })
+      warnings: false,
+      pure_getters: true,
+      unsafe: true,
+      unsafe_comps: true,
+      screw_ie8: true,
+    },
+    output: {
+      comments: false,
+    },
+  }),
+  new webpack.NoEmitOnErrorsPlugin(),
+  // new CompressionPlugin({
+  //   asset: '[path].gz[query]',
+  //   algorithm: 'gzip',
+  //   test: /\.(js|css|html)$/,
+  //   threshold: 0,
+  //   minRatio: 0.8,
+  // }),
 ])
 
 config.module.loaders.push(
-  { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' }
+  { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' }
 )
 
 module.exports = config

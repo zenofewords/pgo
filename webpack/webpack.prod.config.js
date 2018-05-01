4a5ab826ada298e874410f6a99fb447ffc116755
Dominik Zen <zen.dominik@gmail.com>
2018-05-02 01:15:03 +0200

    Update static assets

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
 var config = require('./webpack.base.config.js')
 var CompressionPlugin = require("compression-webpack-plugin")
+var MinifyPlugin = require("babel-minify-webpack-plugin");
 
 
 config.output.filename = '[name]-[hash].js',
@@ -14,19 +15,7 @@ config.plugins = config.plugins.concat([
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': '"production"',
   }),
-  new webpack.optimize.UglifyJsPlugin({
-    mangle: true,
-    compressor: {
-      warnings: false,
-      pure_getters: true,
-      unsafe: true,
-      unsafe_comps: true,
-      screw_ie8: true,
-    },
-    output: {
-      comments: false,
-    },
-  }),
+  new MinifyPlugin(),
   new webpack.NoEmitOnErrorsPlugin(),
   // new CompressionPlugin({
   //   asset: '[path].gz[query]',

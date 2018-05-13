504682f674f496945c1920d0fb568cca87426e5b
Dominik Zen <zen.dominik@gmail.com>
2018-05-13 16:30:34 +0200

    Refactor API

--- pgo/api/urls.py
+++ pgo/api/urls.py
@@ -3,18 +3,12 @@ from __future__ import unicode_literals
 from django.conf.urls import url
 
 from pgo.api.routers import pgo_router
-from pgo.api.views import (
-    BreakpointCalcAPIView,
-    BreakpointCalcDetailAPIView,
-    GoodToGoAPIView,
-)
+from pgo.api.views import BreakpointCalcAPIView, GoodToGoAPIView
 
 
 pgo_api_urls = pgo_router.urls
 pgo_api_urls.extend((
     url(r'^breakpoint-calc/$',
         BreakpointCalcAPIView.as_view(), name='breakpoint-calc'),
-    url(r'^breakpoint-calc-detail/$',
-        BreakpointCalcDetailAPIView.as_view(), name='breakpoint-calc-detail'),
     url(r'^good-to-go/$', GoodToGoAPIView.as_view(), name='good-to-go'),
 ))
